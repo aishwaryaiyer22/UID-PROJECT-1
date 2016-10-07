ac4f71b0bfa867f209f6971335832e7fc6236693
aishwaryaiyer22 <ai2336@columbia.edu>
2016-10-07 01:00:02 -0400

    open new dowcument and modify it through javascript-test

--- part_2/get_info.js
+++ part_2/get_info.js
@@ -11,29 +11,52 @@
 // }
 GOOGLE_API_KEY = "AIzaSyDURNcNFnKTwcwtgO9-x4JUs3g1HT_H9RI"
 //var address = "1263 Pacific Ave. Kansas City, KS"
-function getInfo_test(){
-	console.log(address);
+function getInfo(){
 	var string = "";
 	var finalUrl = "https://www.googleapis.com/civicinfo/v2/representatives";
-	if (document.getElementById("address")!= null) {
-		finalUrl += "?address="+document.getElementById("address").value;
-		console.log(document.getElementById("address").value);
+	if (document.getElementById("Address").value!= "") {
+		finalUrl += "?address="+document.getElementById("Address").value;
+		// console.log(document.getElementById("Address").value);
 	}
-	if (document.getElementById("offices")!= null) 
-		finalUrl += "&includeOffices="+document.getElementById("offices").value;
-	if (document.getElementById("levels")!= null) 
-		finalUrl += "&levels="+document.getElementById("levels").value;
-	if (document.getElementById("roles") != null) 
-		finalUrl += "&roles="+document.getElementById("offices");
+	else {
+		window.alert("Please enter address.");
+		return;
+	}	
+	if (document.getElementById("includeOffice")!= null)  
+		finalUrl += "&includeOffices="+document.getElementById("includeOffice").value;
+	
+	if (document.getElementById("levels")!= null) {
+		var select1 = document.getElementById("levels");
+	    for (var i = 0; i < select1.length; i++) {
+	        if (select1.options[i].selected) 
+	        	finalUrl += "&levels="+select1.options[i].value;
+	    }
+    	// console.log(finalUrl);
+	}
+
+		
+	if (document.getElementById("roles") != null) {
+		var select1 = document.getElementById("roles");
+	    for (var i = 0; i < select1.length; i++) {
+	        if (select1.options[i].selected) 
+	        	finalUrl += "&roles="+select1.options[i].value;
+	    }
+    	// console.log(finalUrl);
+	}
+		
 	finalUrl += "&key="+GOOGLE_API_KEY
 	console.log("finalUrl "+finalUrl);
 	//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;	
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 	  if (this.readyState == 4 && this.status == 200) {
-	  		string = JSON.stringify(this.responseText);
+	  		string = JSON.parse(this.responseText);
+	  		// stringified = JSON.stringify(this.responseText);
 	  		document.getElementById("output").innerHTML = "Success";
-	  		console.log(string);
+	  		document.getElementById("loadingbutton").innerHTML = "<img src=tumblr_nh1mchoRUh1sqqx06o1_500.gif style=width:40px;height:40px;align:center;>";
+	  		document.getElementById("formpage").style.display = "hidden";
+	  		open_display(string);
+	  		// console.log(stringified);
 	  }
 	  else {
 	  	console.log(this.status);
